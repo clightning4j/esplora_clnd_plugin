25addf96773e8e08083d748fae55bc02ec7688bc
Luca Vaccaro <me@lvaccaro.com>
2020-08-27 10:42:15 +0200

    Support empty fee_rate estimation api response

--- esplora.c
+++ esplora.c
@@ -278,7 +278,7 @@ static struct command_result *estimatefees(struct command *cmd,
   bool valid;
   // slow, normal, urgent, very_urgent
   int targets[4] = {144, 5, 3, 2};
-  u64 feerates[4] = {0, 0, 0, 0};
+  u64 feerates[4] = {1, 0, 0, 0};
 
   if (!param(cmd, buf, toks, NULL))
     return command_param_failed();
@@ -311,9 +311,8 @@ static struct command_result *estimatefees(struct command *cmd,
                     cmd->methodname, targets[i], (int)sizeof(feerate_res),
                     feerate_res);
       plugin_log(cmd->plugin, LOG_INFORM, "err: %s", err);
-      if (i == 0)
-        return command_done_err(cmd, BCLI_ERROR, err, NULL);
-      feerates[i] = feerates[i - 1];
+      if (i > 0)
+        feerates[i] = feerates[i - 1];
     }
 
     // ... But lightningd wants a sat/kVB feerate, divide by 10**4 !
