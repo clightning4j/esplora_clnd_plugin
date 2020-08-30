99acfc9d41d2b3eb815d3060b87b58b0ac633889
Antoine Poinsot <darosior@protonmail.com>
2020-08-30 23:08:25 +0200

    esplora: use tal_hex() in getutxout
    Signed-off-by: Antoine Poinsot <darosior@protonmail.com>

--- esplora.c
+++ esplora.c
@@ -462,8 +462,7 @@ static struct command_result *getutxout(struct command *cmd, const char *buf,
   // replay response
   response = jsonrpc_stream_success(cmd);
   json_add_amount_sat_only(response, "amount", output.amount);
-  json_add_string(response, "script",
-                  tal_hexstr(response, output.script, sizeof(output.script)));
+  json_add_string(response, "script", tal_hex(response, output.script));
 
   return command_finished(cmd, response);
 }
